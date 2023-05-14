4579485299e117d8d13a761bef8c7d74887c5d3f
sadgh77 <sadeghhadipour1@gmail.com>
2023-05-14 13:15:41 +0330

    add i18n

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,16 @@ export default {
 };
 </script>
 <style>
-.main{
+@font-face {
+  font-family: "iranSans";
+  src: local("iranSans"),
+  url(assets/font/FontsFree-Net-ir_sans.ttf) format("truetype");
+}
+
+.main {
+  font-family: iranSans, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   background-color: #f1f1f1;
 }
 </style>
--- src/components/NaveBar.vue
+++ src/components/NaveBar.vue
@@ -20,23 +20,40 @@
     </div>
 
     <v-spacer></v-spacer>
-
-<!--    <v-btn-->
-<!--        color="#d1e7ff"-->
-<!--        depressed-->
-<!--        outlined-->
-<!--        rounded-->
-<!--        to="/projects-page"-->
-<!--    >-->
-<!--      Projects-->
-<!--    </v-btn>-->
+    <v-select
+        class="select mr-2"
+        v-model="selectedLang"
+        @change="changeLocale"
+        :items="languages"
+    ></v-select>
+    <!--    <v-btn-->
+    <!--        color="#d1e7ff"-->
+    <!--        depressed-->
+    <!--        outlined-->
+    <!--        rounded-->
+    <!--        to="/projects-page"-->
+    <!--    >-->
+    <!--      Projects-->
+    <!--    </v-btn>-->
   </v-app-bar>
 </template>
 
 <script>
+import i18n from "@/i18n";
+
 export default {
   name: "NaveBar",
+  data() {
+    return {
+      selectedLang: i18n.locale,
+      languages: ['en', 'fa']
+    }
+  },
   methods: {
+    changeLocale() {
+      i18n.locale = this.selectedLang
+      this.$vuetify.rtl = !this.$vuetify.rtl
+    },
     goto(routeName) {
       if (this.$route.name !== 'home') {
         this.$router.push(routeName)
@@ -55,5 +72,13 @@ export default {
 .logo a {
   color: #d1e7ff;
   text-decoration: none;
+}
+
+.select {
+  max-width: 80px;
+  max-height: 80px;
+  font-size: 15px;
+  font-weight: bold;
+  color: #d1e7ff;
 }
 </style>
