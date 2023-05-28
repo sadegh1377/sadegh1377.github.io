2581eca04f356695e85176adbeab5258f985248e
sadeg1377 <sadeghhadipour1@gmail.com>
2023-05-28 12:39:42 +0330

    add light and dark theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,18 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#1976d2',
+                secondary: '#f1f1f1',
+                third: '#fafafa',
+            },
+            dark: {
+                primary: '#090000',
+                secondary: '#686666',
+                third: '#4f4f4f',
+            }
+        },
+    },
 });
