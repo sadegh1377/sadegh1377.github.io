d94f47e8e786fa43dbf5e66a93c72ec0ac3cf490
sadeg1377 <sadeghhadipour1@gmail.com>
2024-12-07 16:24:31 +0330

    refactor about property and break it down and some css changes

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,152 +1,115 @@
 <template>
-    <div class="text-center mt-15 mb-15 homePage">
-        <v-container class="third rounded">
-            <v-row>
-                <v-col
-                    cols="12"
-                    xs="6"
-                    sm="5"
-                    md="4"
-                >
-                    <v-card
-                        class="pa-2 rounded"
-                        outlined
-                        tile>
-                        <img class="profileImage elevation-5" src="../assets/profilePhoto.jpg" alt="Profile Image">
-                        <v-card-title class="justify-center pt-2">
-                            {{ $t('homePage.name') }}
-                        </v-card-title>
-                        <v-card-subtitle class="">{{ $t('homePage.occupation') }}</v-card-subtitle>
-                        <v-card-text class="body-1 summary" v-html="$t('homePage.summary')">
-                        </v-card-text>
-                        <v-card-title class="" style="text-align: left">
-                            {{ $t("homePage.skillsTitle") }}
-                        </v-card-title>
-                        <v-card-text style="text-align: left" class="d-flex justify-start ">
-                            <ul>
-                                <li class="body-1">JavaScript</li>
-                                <li class="body-1">VueJs</li>
-                                <li class="body-1">VueRouter</li>
-                                <li class="body-1">VuetifyJs</li>
-                                <li class="body-1">Quasar</li>
-                                <li class="body-1">NuxtJs</li>
-                                <li class="body-1">Angular</li>
-                                <li class="body-1">React</li>
-                                <li class="body-1">HTML</li>
-                                <li class="body-1">CSS</li>
-                                <li class="body-1">Bootstrap</li>
-                                <li class="body-1">TailWindCss</li>
-                                <li class="body-1">Figma</li>
-                            </ul>
-                            <ul>
-                                <li class="body-1">NodeJs</li>
-                                <li class="body-1">ExpressJs</li>
-                                <li class="body-1">MongoDB</li>
-                                <li class="body-1">Mongoose</li>
-                                <li class="body-1">Firebase</li>
-                                <li class="body-1">ChartJs</li>
-                                <li class="body-1">Git</li>
-                                <li class="body-1">i18n</li>
-                            </ul>
-                        </v-card-text>
-                        <v-card-title class="" style="text-align: left">
-                            {{ $t("homePage.otherSkillsTitle") }}
-                        </v-card-title>
-                        <v-card-text :class="{'text-left': $i18n.locale === 'en','text-right': $i18n.locale === 'fa'}"
-                                     v-html="$t('homePage.otherSkills')">
-                        </v-card-text>
-                        <v-card-title class="" style="text-align: left">
-                            {{ $t("homePage.projectsTitle") }}
-                        </v-card-title>
-                        <v-card-text>
-                            <ul>
-                                <li><a href="https://github.com/sadegh1377/news-client" target="_blank">news-client</a>
-                                </li>
-                                <li><a href="https://github.com/sadegh1377/ecommProject"
-                                       target="_blank">ecommProject</a>
-                                </li>
-                                <li><a href="https://github.com/sadegh1377/movie-details"
-                                       target="_blank">movie-details</a>
-                                </li>
-                            </ul>
-                        </v-card-text>
-                        <v-card-title class="" style="text-align: left">
-                            {{ $t("homePage.educationHistoryTitle") }}
-                        </v-card-title>
-                        <v-card-text :class="{'text-left': $i18n.locale === 'en','text-right': $i18n.locale === 'fa'}"
-                                     v-html="$t('homePage.educationHistory')">
-
-                        </v-card-text>
-                        <v-card-actions>
-                            <a href="https://github.com/sadegh1377" target="_blank"
-                               title="GitHub">
-                                <v-icon class="mr-2">mdi-github</v-icon>
-                            </a>
-                            <a href="https://www.linkedin.com/in/sadegh-hadipour-b3b62a192/" target="_blank"
-                               title="Linkedin">
-                                <v-icon class="mr-2">mdi-linkedin</v-icon>
-                            </a>
-                            <v-icon :class="{'mr-2': $i18n.locale === 'fa'}" title="Phone Number">mdi-phone</v-icon>
-                            <span class="body-1">{{ $t("homePage.phoneNumber") }}</span>
-                        </v-card-actions>
-                    </v-card>
-                </v-col>
-                <v-col
-                    class="about"
-                    cols="12"
-                    xs="6"
-                    sm="7"
-                    md="8">
-                    <v-card
-                        class="pa-4 rounded"
-                        outlined
-                        tile
-                        v-html="$t('homePage.about')">
-                    </v-card>
-                </v-col>
-            </v-row>
-        </v-container>
-    </div>
+  <div class="text-center mt-15 mb-15 homePage">
+    <v-container class="third rounded">
+      <v-row>
+        <v-col
+            cols="12"
+            xs="6"
+            sm="5"
+            md="4"
+        >
+          <SideBar/>
+        </v-col>
+        <v-col
+            class="about"
+            cols="12"
+            xs="6"
+            sm="7"
+            md="8">
+          <v-card
+              class="pa-4 rounded"
+              outlined
+              tile>
+            <p class="body-1">
+              {{ $t("homePage.about") }}</p>
+            <b>{{ $t("homePage.workExperienceTitle") }}</b>
+            <p class="body-2 ">
+              <span class="font-weight-bold">{{ $t("homePage.freelance") }}</span>
+              <br>
+              <span class="font-italic">
+                            {{ $t("homePage.secondFreeLanceDuration") }}
+                            </span>
+            </p>
+            <ul class="px-5 mb-3 body-2">
+              <li  v-for="(detail, index) in $t('homePage.secondFreelanceDetail')" :key="index" v-html="detail">
+              </li>
+            </ul>
+            <p class="body-2">
+              <span class="font-weight-bold">{{ $t("homePage.frontDeveloper") }}</span>
+              <br>
+              <span v-html="$t('homePage.filioSummary')"></span>
+              <br>
+              <span class="font-italic">
+                            {{ $t("homePage.filioDuration") }}
+                            </span>
+            </p>
+            <ul class="px-5 mb-3 body-2">
+              <li v-for="(detail, index) in $t('homePage.filioDetail')" :key="index">{{ detail }}</li>
+            </ul>
+            <p class="body-2 ">
+              <span class="font-weight-bold">{{ $t("homePage.freelance") }}</span>
+              <br>
+              <span class="font-italic">
+                            {{ $t("homePage.firstFreeLanceDuration") }}
+                            </span>
+            </p>
+            <ul class="px-5 mb-3 body-2" v-for="(detail, index) in $t('homePage.fistFreelanceDetail')" :key="index">
+              <li v-html="detail">
+              </li>
+            </ul>
+            <p class="body-2 ">
+              <span class="font-weight-bold">{{ $t("homePage.juniorTitle") }}</span>
+              <br>
+              {{ $t("homePage.superzSummary") }}
+              <br>
+              <span class="font-italic">
+                            {{ $t("homePage.superzDuration") }}
+                            </span>
+            </p>
+            <ul class="px-5 mb-3 body-2">
+              <li v-for="(detail, index) in $t('homePage.superzDetail')" :key="index">{{detail}}</li>
+            </ul>
+            <p class="body-2 ">
+              <span class="font-weight-bold">{{$t('homePage.internTitle')}}</span>
+              <br>
+              {{ $t("homePage.superzSummary") }}
+              <br>
+              <span class="font-italic">
+                           {{$t("homePage.internDuration")}}
+                            </span>
+            </p>
+              <ul class="px-5 body-2">
+                <li v-for="(detail, index) in $t('homePage.internDetail')" :key="index">{{detail}}</li>
+              </ul>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
 
+import SideBar from "@/components/SideBar.vue";
+
 export default {
-    name: 'HomeView',
-    methods: {}
+  name: 'HomeView',
+  components: {SideBar},
+  methods: {}
 
 }
 </script>
 <style scoped>
 
-.profileImage {
-    display: inline-block;
-    margin: 0 auto;
-    width: 180px;
-    height: 180px;
-    border: 1px solid gray;
-    border-radius: 50% !important;
-    aspect-ratio: auto 100 / 100;
-}
-
-.summary {
-    text-align: justify;
-    line-height: 20px;
-}
-
 .about {
-    text-align: justify;
-}
-
-li {
-    text-align: justify;
+  text-align: justify;
 }
 
 .lineHeight {
-    line-height: 1.2 !important;
+  line-height: 1.2 !important;
 }
 
 .darkLinks {
-    color: #FFFBF5 !important;
+  color: #FFFBF5 !important;
 }
 </style>
